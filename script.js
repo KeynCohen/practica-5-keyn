5bc26e4fc9bb34ec9d63b4bc790ebf6baf556f41
KeynCohen <kcoh500@g.educaand.es>
2026-05-20 10:01:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let contador = 0;
-
 const numero = document.querySelector("#numero");
-
 const botonSumar = document.querySelector("#btn-sumar");
 const botonRestar = document.querySelector("#btn-restar");
 const botonReset = document.querySelector("#btn-reset");
@@ -10,32 +8,30 @@ function pintarContador() {
   numero.textContent = contador;
 }
 
-botonSumar.addEventListener("click", function () {
-  contador++;
+  botonSumar.addEventListener("click", function () {
+    contador++;
+    pintarContador();
+  });
+
+  botonRestar.addEventListener("click", function () {
+    contador--;
+    pintarContador();
+  });
+
+  botonReset.addEventListener("click", function () {
+    contador = 0;
   pintarContador();
-});
+  });
 
-botonRestar.addEventListener("click", function () {
-  contador--;
-  pintarContador();
-});
+  botonSaludar.addEventListener("click", function () {
+    mensaje.textContent = "¡Hola!";
+  });
 
-botonReset.addEventListener("click", function () {
-  contador = 0;
-  pintarContador();
-});
-
-botonSaludar.addEventListener("click", function () {
-  mensaje.textContent = "¡Hola!";
-});
-
-const mensaje = document.querySelector("#mensaje");
-
-const inputTarea = document.querySelector("#input-tarea");
-const botonAgregar = document.querySelector("#btn-agregar");
-const lista = document.querySelector("#lista-tareas");
-
-const tareas = [];
+    const mensaje = document.querySelector("#mensaje");
+    const inputTarea = document.querySelector("#input-tarea");
+    const botonAgregar = document.querySelector("#btn-agregar");
+    const lista = document.querySelector("#lista-tareas");
+    const tareas = [];
 
 function renderTareas() {
   lista.innerHTML = "";
@@ -47,13 +43,13 @@ function renderTareas() {
   }
 }
 
-botonAgregar.addEventListener("click", function () {
-  const texto = inputTarea.value.trim();
-
-  if (texto === "") {
-    mensaje.textContent = "Escribe una tarea antes de agregar.";
-    return;
-  }
+  botonAgregar.addEventListener("click", function () {
+    const texto = inputTarea.value.trim();
+  
+    if (texto === "") {
+      mensaje.textContent = "Escribe una tarea antes de agregar.";
+      return;
+    }
 
   tareas.push(texto);
   renderTareas();
@@ -61,8 +57,6 @@ botonAgregar.addEventListener("click", function () {
   inputTarea.value = "";
   mensaje.textContent = "Tarea agregada correctamente.";
 });
-
-
 const botonTema = document.querySelector("#btn-tema");
 const panel = document.querySelector(".panel");
 
